8d86602719a3a5d8aad46e18127bc0bb96c59383
Maximo <pachecomaximo11@gmail.com>
2022-08-09 19:29:05 -0300

    register validations

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,9 @@
-import { AuthLayout  } from "../layouts"
-import { Box, Button, Grid, TextField, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
 import { Formik } from "formik";
+import { Box, Button, Grid, Typography } from '@mui/material';
+import { AuthLayout  } from "../layouts"
+import { MyTextInput } from "../components/input";
+import { form } from "../utils";
 
 export const RegisterPage = () => {
 	return (
@@ -14,62 +16,66 @@ export const RegisterPage = () => {
 						password: '',
 						password2: ''
 					}}
-					onSubmit={ () => {}}
+					onSubmit={ (values) => {
+						console.log(values);
+						
+					}}
+					validationSchema={ form.registerValidations }
 				>
+					{
+						({ handleSubmit }) => (
+							<form onSubmit={ handleSubmit }>
+								<Grid container spacing={ 3 }>
+									<Grid item xs={ 12 }>
+										<Typography variant="h1" component="h1" fontSize={ 25 } textAlign="center">Crear cuenta</Typography>
+									</Grid>
+									<Grid item xs={ 12 }>
+										<MyTextInput 
+											label="Full name"
+											name="name"
+										/>
+									</Grid>
+									<Grid item xs={ 12 }>
+										<MyTextInput 
+											type="email"
+											name="email"
+											label="Email"
+										/>
+									</Grid>
+									<Grid item xs={ 12 }>
+										<MyTextInput 
+											label="Password"
+											type="password"
+											name="password"
+										/>
+									</Grid>
+									<Grid item xs={ 12 }>
+										<MyTextInput 
+											label="Confirm Password"
+											type="password"
+											name="password2"
+										/>
+									</Grid>
+									<Grid item xs={ 12 } >
+										<Button fullWidth variant="contained" color="error" type="submit">
+											<Typography>Ingresar</Typography>
+										</Button>
 
-					<Grid container spacing={ 3 }>
-						<Grid item xs={ 12 }>
-							<Typography variant="h1" component="h1" fontSize={ 25 } textAlign="center">Crear cuenta</Typography>
-						</Grid>
-						<Grid item xs={ 12 }>
-							<TextField 
-								fullWidth
-								label="Email"
-								color="error"
-								variant="outlined"
-							/>
-						</Grid>
-						<Grid item xs={ 12 }>
-							<TextField 
-								fullWidth
-								label="Nombre de usuario"
-								color="error"
-								variant="outlined"
-							/>
-						</Grid>
-						<Grid item xs={ 12 }>
-							<TextField 
-								fullWidth
-								label="Password"
-								color="error"
-								variant="outlined"
-								type="password"
-							/>
-						</Grid>
-						<Grid item xs={ 12 }>
-							<TextField 
-								fullWidth
-								label="Confirm Password"
-								color="error"
-								variant="outlined"
-								type="password"
-							/>
-						</Grid>
-						<Grid item xs={ 12 } >
-							<Button fullWidth variant="contained" color="error">
-								<Typography>Ingresar</Typography>
-							</Button>
+										<Link to="/login" style={{ color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+											<Typography textAlign="center" mt={2} fontSize={15} sx={{
+												":hover":{
+													textDecoration: 'underline'
+												}
+											}}>¿Ya tienes una cuenta? Inicia Sesión</Typography>
+										</Link>
+									</Grid>
 
-							<Link to="/login" style={{ color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
-								<Typography textAlign="center" mt={2} fontSize={15} sx={{
-									":hover":{
-										textDecoration: 'underline'
-									}
-								}}>¿Ya tienes una cuenta? Inicia Sesión</Typography>
-							</Link>
-						</Grid>
+								</Grid>
+								
+							</form>
+						)
 
-					</Grid>
+					}
 				</Formik>
 
 			</Box>	
--- src/utils/form.ts
+++ src/utils/form.ts
@@ -8,3 +8,13 @@ export const loginValidation = Yup.object({
 			.oneOf([Yup.ref('password'), null], 'Passwords must match')
 			.required('Required'),
 })
+
+export const registerValidations = Yup.object({
+	name: Yup.string().min(2, 'Minimum is 2 characters').required('Required'),
+	email: Yup.string().email('Invalid email adress').required('Required'),
+	password: Yup.string().min(6, 'Minimum is 6 characters').required('Required'),
+	password2: Yup.string()
+		.min(6, 'Minimum is 6 characters')
+		.oneOf([Yup.ref('password'), null], 'Passwords must match')
+		.required('Required'),
+})
